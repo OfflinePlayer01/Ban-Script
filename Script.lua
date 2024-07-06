3131248108ebb0d34077e796cc0995296e4b7b08
OfflinePlayer01 <168975740+OfflinePlayer01@users.noreply.github.com>
2024-07-06 22:37:19 +0300

    Script.lua

---
+++ Script.lua
@@ -0,0 +1,97 @@
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = game.Players.LocalPlayer.PlayerGui
+
+local textBox = Instance.new("TextBox")
+textBox.Size = UDim2.new(1, 1, 1, 1)
+textBox.Position = UDim2.new(0, 0, 0, 0)
+textBox.BackgroundColor3 = Color3.new(1, 1, 1)
+textBox.BorderSizePixel = 0
+textBox.BackgroundTransparency = 0.8
+textBox.FontSize = Enum.FontSize.Size14
+textBox.TextColor3 = Color3.new(1, 1, 1)
+textBox.TextStrokeTransparency = 0
+textBox.TextStrokeColor3 = Color3.new(0, 0, 0)
+textBox.ClearTextOnFocus = false
+textBox.TextEditable = false
+textBox.Text = "〰 Script Executed 〰"
+textBox.Parent = screenGui
+
+local timer = 5
+local timerActive = true
+
+local function startTimer()
+    timerActive = true
+    textBox.Text = "〰 You Will Be Banned in 7 Seconds 〰"
+    wait(timer)
+    timerActive = false
+    textBox.Text = ""
+    textBox:Destroy()
+end
+
+startTimer()
+
+local function onFocusLost(enterPressed, inputObject)
+    if enterPressed and timerActive then
+        print(textBox.Text)
+        startTimer()
+    end
+end
+textBox.FocusLost:Connect(onFocusLost)
+wait(5)
+		    game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "〰 Are You Ready to Be Banned? 〰";
+        Text = "〰 If You Are Banned, Roblox Forgives You 〰";
+        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+    Duration = 3;
+
+		    game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "〰 Script By _offlineplayer_ 〰";
+        Text = "〰 If you are not banned, try again in an hour or two. 〰";
+        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+    Duration = 3;
+wait(2)
+
+local A_1 = "Stupid Guy"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1)
+local A_1 = "Fuck U Bitch"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1)
+local A_1 = "My Dick Is So Good"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(3)
+local A_1 = "Your Dick Is So Good"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1)
+local A_1 = "WTF"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1)
+local A_1 = "Shit"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = "i'm gonna cut your head off and shit down your neck"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = "son of a bitch"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = "BKZ"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
